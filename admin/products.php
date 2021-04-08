af1ce2c83da428a5cd2880dea73897c8ccb7cb24
ranulfjn <ranulfnoronha@gmail.com>
2021-04-07 23:39:51 -0400

    products

--- admin/products.php
+++ admin/products.php
@@ -1,36 +1,36 @@
 <?php session_start(); ?>
-<?php include_once("./templates/top.php"); ?>
-<?php include_once("./templates/navbar.php"); ?>
+<?php include_once './templates/top.php'; ?>
+<?php include_once './templates/navbar.php'; ?>
 <div class="container-fluid">
   <div class="row">
-    
-    <?php include "./templates/sidebar.php"; ?>
 
-      <div class="row">
-      	<div class="col-10">
-      		<h2>Product List</h2>
-      	</div>
-      	<div class="col-2">
-      		<a href="#" data-toggle="modal" data-target="#add_product_modal" class="btn btn-primary btn-sm">Add Product</a>
-      	</div>
+    <?php include './templates/sidebar.php'; ?>
+
+    <div class="row">
+      <div class="col-10">
+        <h2>Product List</h2>
       </div>
-      
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Image</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Category</th>
-              <th>Brand</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody id="product_list">
-            <!-- <tr>
+      <div class="col-2">
+        <a href="#" data-toggle="modal" data-target="#add_product_modal" class="btn btn-primary btn-sm">Add Product</a>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Category</th>
+            <th>Brand</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody id="product_list">
+          <!-- <tr>
               <td>1</td>
               <td>ABC</td>
               <td>FDGR.JPG</td>
@@ -39,9 +39,9 @@
               <td>aPPLE</td>
               <td><a class="btn btn-sm btn-info"></a><a class="btn btn-sm btn-danger">Delete</a></td>
             </tr> -->
-          </tbody>
-        </table>
-      </div>
+        </tbody>
+      </table>
+    </div>
     </main>
   </div>
 </div>
@@ -49,7 +49,8 @@
 
 
 <!-- Add Product Modal start -->
-<div class="modal fade" id="add_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="add_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -60,65 +61,65 @@
       </div>
       <div class="modal-body">
         <form id="add-product-form" enctype="multipart/form-data">
-        	<div class="row">
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Name</label>
-		        		<input type="text" name="product_name" class="form-control" placeholder="Enter Product Name">
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Brand Name</label>
-		        		<select class="form-control brand_list" name="brand_id">
-		        			<option value="">Select Brand</option>
-		        		</select>
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Category Name</label>
-		        		<select class="form-control category_list" name="category_id">
-		        			<option value="">Select Category</option>
-		        		</select>
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Description</label>
-		        		<textarea class="form-control" name="product_desc" placeholder="Enter product desc"></textarea>
-		        	</div>
-        		</div>
+          <div class="row">
+            <div class="col-12">
+              <div class="form-group">
+                <label>Product Name</label>
+                <input type="text" name="product_name" class="form-control" placeholder="Enter Product Name">
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-group">
+                <label>Brand Name</label>
+                <select class="form-control brand_list" name="brand_id">
+                  <option value="">Select Brand</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-group">
+                <label>Category Name</label>
+                <select class="form-control category_list" name="category_id">
+                  <option value="">Select Category</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-group">
+                <label>Product Description</label>
+                <textarea class="form-control" name="product_desc" placeholder="Enter product desc"></textarea>
+              </div>
+            </div>
             <div class="col-12">
               <div class="form-group">
                 <label>Product Qty</label>
                 <input type="number" name="product_qty" class="form-control" placeholder="Enter Product Quantity">
               </div>
             </div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Price</label>
-		        		<input type="number" name="product_price" class="form-control" placeholder="Enter Product Price">
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Keywords <small>(eg: apple, iphone, mobile)</small></label>
-		        		<input type="text" name="product_keywords" class="form-control" placeholder="Enter Product Keywords">
-		        	</div>
-        		</div>
-        		<div class="col-12">
-        			<div class="form-group">
-		        		<label>Product Image <small>(format: jpg, jpeg, png)</small></label>
-		        		<input type="file" name="product_image" class="form-control">
-		        	</div>
-        		</div>
-        		<input type="hidden" name="add_product" value="1">
-        		<div class="col-12">
-        			<button type="button" class="btn btn-primary add-product">Add Product</button>
-        		</div>
-        	</div>
-        	
+            <div class="col-12">
+              <div class="form-group">
+                <label>Product Price</label>
+                <input type="number" name="product_price" class="form-control" placeholder="Enter Product Price">
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-group">
+                <label>Product Keywords </label>
+                <input type="text" name="product_keywords" class="form-control" placeholder="Enter Product Keywords">
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-group">
+                <label>Product Image <small>(format: jpg, jpeg, png)</small></label>
+                <input type="file" name="product_image" class="form-control">
+              </div>
+            </div>
+            <input type="hidden" name="add_product" value="1">
+            <div class="col-12">
+              <button type="button" class="btn btn-primary add-product">Add Product</button>
+            </div>
+          </div>
+
         </form>
       </div>
     </div>
@@ -127,7 +128,8 @@
 <!-- Add Product Modal end -->
 
 <!-- Edit Product Modal start -->
-<div class="modal fade" id="edit_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="edit_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -198,7 +200,7 @@
               <button type="button" class="btn btn-primary submit-edit-product">Add Product</button>
             </div>
           </div>
-          
+
         </form>
       </div>
     </div>
@@ -206,7 +208,7 @@
 </div>
 <!-- Edit Product Modal end -->
 
-<?php include_once("./templates/footer.php"); ?>
+<?php include_once './templates/footer.php'; ?>
 
 
 
